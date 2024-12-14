af6502eab52ea334f288dab76d8a8066441078e0
yohannesalex <yohannesalemayehu624@gmail.com>
2024-12-14 23:06:25 +0300

    fix importing issue

--- scripts/pre_processor.py
+++ scripts/pre_processor.py
@@ -1,4 +1,11 @@
 # Text preprocessing function
+import string
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+
 def preprocess_text(text):
     text = text.lower()
     text = text.translate(str.maketrans('', '', string.punctuation))  # Remove punctuation
